1cf270586c30cf3f9b3a27b8634316918a652c99
Kay Putra <kaspr.works@gmail.com>
2022-05-26 23:29:00 +1000

    add basic game logic

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Hi");
+// step 1: grab the things that will change in the game
+
+const guess = document.querySelector(".guess");
+// const number = document.querySelector(".number").textContent;
+const message = document.querySelector(".message");
+// const score = document.querySelector(".score").textContent;
+// const highscore = document.querySelector(".highscore").textContent;
+const checkBtn = document.querySelector(".check");
+
+// step 2: generate random number
+//const randomNumber = Math.trunc(Math.random() * 20) + 1;
+const randomNumber = 10;
+let score = 20;
+let highScore;
+
+// step 3: add event listener to check button
+checkBtn.addEventListener("click", function () {
+  if (+guess.value === randomNumber) {
+    message.textContent = "win";
+    highScore = score;
+    document.querySelector(".highscore").textContent = highScore;
+  } else if (+guess.value < randomNumber) {
+    message.textContent = "too low";
+    score--;
+  } else if (+guess.value > randomNumber) {
+    message.textContent = "too high";
+    score--;
+  }
+
+  document.querySelector(".score").textContent = score;
+});
